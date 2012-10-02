19406305788b23d518742607138eec2336315b0b
BryIsAZombie <bryanzettler@gmail.com>
2012-10-02 13:03:47 -0500

    Update to todoify.js

--- todoify/javascript/todoify.js
+++ todoify/javascript/todoify.js
@@ -1,6 +1,15 @@
 //Define vars
 var i = 0;
 
+function toggleVis(id){
+	var e = document.getElementById(id);
+	if(e.style.display == 'block'){
+		e.style.display = 'none';
+	}
+	else {
+		e.style.display = 'block';
+	}
+}
 function addItem(e) {
     if (e.keyCode == 13) {
 	i++;
@@ -9,6 +18,7 @@ function addItem(e) {
 	document.getElementById('tasks').innerHTML += listed;
 	return false;
     }
+    e.style.display = 'none';
 }
 
 
