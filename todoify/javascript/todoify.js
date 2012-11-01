9dcdfa1bfcf787cbab22cb164d9a5a2386e71005
BryIsAZombie <bryanzettler@gmail.com>
2012-11-01 01:35:07 -0500

    Added radio buttons for priority and created getPriority function.

--- todoify/javascript/todoify.js
+++ todoify/javascript/todoify.js
@@ -1,10 +1,30 @@
 //Define vars
 var i = 0;
+	
+function getPriority(){
+	var high = document.getElementById('high'),
+		medium = document.getElementById('medium'),
+		low = document.getElementById('low');
+	if (high.checked){
+		priority = "high";
+	}
+	else if (medium.checked){
+		priority = "medium";
+	}
+	else if (low.checked){
+		priority = "low";
+	}
+	else {
+		priority = "defualt";
+	}
+	return priority;
+}
 
 function addItem(e) {
 	i++;
-	var listItem = document.getElementById('todo').value,
-		listed = '<li id="item' + i + '"><span class="priority defualt"></span><a href="#" class="closex" onclick="removeItem(\'item'+ i + '\')"></a><span>' + listItem + '</span></li>';
+	var priority = getPriority(),
+		listItem = document.getElementById('todo').value,
+		listed = '<li id="item' + i + '"><span class="priority '+ priority + '"></span><a href="#" class="closex" onclick="removeItem(\'item'+ i + '\')"></a><span>' + listItem + '</span></li>';
 	document.getElementById('tasks').innerHTML += listed;
 	e.preventDefault();
 }
